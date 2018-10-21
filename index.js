33998a6d7cbcdd5f6fd5551bb6e9e9765c7075e5
akashhonmukhe <achonmukhe@gmail.com>
2018-10-21 17:26:52 +0530

    Index File Created

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var url = require('url')
+
+var httpServer = http.createServer(function(req, res){
+    
+    //res.end('Hello World')
+})
+
+
+httpServer.listen(3000, function(){
+    console.log('Connected to port 3000')
+})
